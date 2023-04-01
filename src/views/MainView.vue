4227594fd471bb0197eb038ca357099545bdff80
Konstantin Savusia <savconstantine@gmail.com>
2023-04-01 13:24:44 +0600

    main view

---
+++ src/views/MainView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import IphonePhotoAcademyCopyBlock from "@/modules/IphonePhotoAcademyCopyBlock/index.vue";
+</script>
+
+<template>
+  <div
+    class="flex w-full max-w-[393px] flex-col items-start justify-start gap-4 md:flex-row"
+  >
+    <IphonePhotoAcademyCopyBlock />
+    <div>iphone mockup</div>
+  </div>
+</template>
