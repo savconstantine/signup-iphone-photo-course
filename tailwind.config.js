692e4eb8e15f954c1111e111bfb2cffed9f74ea4
Konstantin Savusia <savconstantine@gmail.com>
2023-04-01 08:16:20 +0600

    connected fonts open sans and monserrat

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      serif: ["Montserrat", ...defaultTheme.fontFamily.serif],
+      mono: [...defaultTheme.fontFamily.mono],
+    },
   },
   plugins: [],
 };
