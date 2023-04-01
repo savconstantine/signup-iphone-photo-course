9f681b772c32bde8630d225fcaa34eabfc44409c
Konstantin Savusia <savconstantine@gmail.com>
2023-04-01 13:22:15 +0600

    signup form module

---
+++ src/modules/IphonePhotoAcademyCopyBlock/SignUpForm.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Btn from "@/ui/Btn.vue";
+import EmailInputField from "@/ui/EmailInputField.vue";
+</script>
+
+<template>
+  <div class="w-full">
+    <div
+      class="mb-[28px] text-[22px] font-normal leading-[30px] text-[#EEECEC]"
+    >
+      Sign up now to get notified <br />when this course is available!
+    </div>
+    <EmailInputField />
+    <Btn>Please Notify Me</Btn>
+  </div>
+</template>
