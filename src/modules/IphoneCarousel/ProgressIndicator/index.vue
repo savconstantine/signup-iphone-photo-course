1e0b5c0ba62822cafa60d37714a4af156cbfde0a
Konstantin Savusia <savconstantine@gmail.com>
2023-04-01 14:41:13 +0600

    prograss bar slider indicator

---
+++ src/modules/IphoneCarousel/ProgressIndicator/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Item from "./Item.vue";
+import { ref } from "vue";
+const completed = ref(0);
+setInterval(
+  () => (completed.value = Math.floor(Math.random() * 100) + 1),
+  2000
+);
+</script>
+
+<template>
+  <div class="flex w-full justify-center gap-[8px] pb-[30px] pt-[15px]">
+    <Item :completed="completed" />
+    <Item :completed="completed" />
+    <Item :completed="completed" />
+    <Item :completed="completed" />
+  </div>
+</template>
