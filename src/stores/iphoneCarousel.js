0af0c2ef3644839c0ee1667a5e8887696f51ffaf
Konstantin Savusia <savconstantine@gmail.com>
2023-04-01 22:51:26 +0600

    iphoneCarousel store added actions for infinite slide images

--- src/stores/iphoneCarousel.js
+++ src/stores/iphoneCarousel.js
@@ -1,44 +1,29 @@
 import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore({
+export const useIphoneCarousel = defineStore({
   id: "IphoneCarousel",
   state: () => ({
-    currentSlide: 0,
+    timeoutId: null,
+    currentSlide: 3,
     slides: [
       {
         id: 1,
-        src: "@/assets/iPhoneCarousel/carousel-photo-01.jpg",
-        srcset: [
-          "@/assets/iPhoneCarousel/carousel-photo-01@2x.jpg 2x",
-          "@/assets/iPhoneCarousel/carousel-photo-01@3x.jpg 3x",
-        ],
+        name: "carousel-photo-01",
         alt: "carousel-photo-01",
       },
       {
         id: 2,
-        src: "@/assets/iPhoneCarousel/carousel-photo-02.jpg",
-        srcset: [
-          "@/assets/iPhoneCarousel/carousel-photo-02@2x.jpg 2x",
-          "@/assets/iPhoneCarousel/carousel-photo-02@3x.jpg 3x",
-        ],
+        name: "carousel-photo-02",
         alt: "carousel-photo-02",
       },
       {
         id: 3,
-        src: "@/assets/iPhoneCarousel/carousel-photo-03.jpg",
-        srcset: [
-          "@/assets/iPhoneCarousel/carousel-photo-03@2x.jpg 2x",
-          "@/assets/iPhoneCarousel/carousel-photo-03@3x.jpg 3x",
-        ],
+        name: "carousel-photo-03",
         alt: "carousel-photo-03",
       },
       {
         id: 4,
-        src: "@/assets/iPhoneCarousel/carousel-photo-04.jpg",
-        srcset: [
-          "@/assets/iPhoneCarousel/carousel-photo-04@2x.jpg 2x",
-          "@/assets/iPhoneCarousel/carousel-photo-04@3x.jpg 3x",
-        ],
+        name: "carousel-photo-04",
         alt: "carousel-photo-04",
       },
     ],
@@ -47,6 +32,38 @@ export const useCounterStore = defineStore({
     getSlides() {
       return this.slides;
     },
+    getCurrentSlide() {
+      return this.currentSlide;
+    },
+    getCurrentSlideObject() {
+      return this.slides[this.getCurrentSlide()];
+    },
+    getPreviousSlideObject() {
+      return this.slides[
+        this.getCurrentSlide() - 1 < 0
+          ? this.slides.length - 1
+          : this.getCurrentSlide() - 1
+      ];
+    },
   },
-  actions: {},
+  actions: {
+    rollTheSlides() {
+      this.clearTimeoutId();
+      const intervalFunction = () => {
+        this.currentSlide =
+          this.currentSlide === this.slides.length - 1
+            ? 0
+            : this.currentSlide + 1;
+
+            console.warn("store this.currentSlide", this.currentSlide);
+        this.timeoutId = setTimeout(() => {
+          intervalFunction();
+        }, 8000);
+      };
+      intervalFunction();
+    },
+    clearTimeoutId() {
+      clearTimeout(this.timeoutId);
+    },
+  },
 });
