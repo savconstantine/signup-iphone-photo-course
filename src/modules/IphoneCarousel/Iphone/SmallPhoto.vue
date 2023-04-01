856d50f54abb783422287508765d0c23fc29dc34
Konstantin Savusia <savconstantine@gmail.com>
2023-04-01 22:52:46 +0600

    smallphoto component

---
+++ src/modules/IphoneCarousel/Iphone/SmallPhoto.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { defineProps, computed } from "vue";
+const props = defineProps({
+  slide: {
+    type: Object,
+  },
+});
+
+const mainSrc = computed(() => {
+  return new URL(
+    `/src/assets/iPhoneCarousel/small-${props.slide.name}.jpg`,
+    import.meta.url
+  );
+});
+
+const scaleArray = [2, 3];
+
+const mainSrcset = computed(() => {
+  return scaleArray.reduce((acc, scale) => {
+    acc = acc.length === 0 ? acc : `${acc}, `;
+    return `${acc}${new URL(
+      `/src/assets/iPhoneCarousel/small-${props.slide.name}@${scale}x.jpg`,
+      import.meta.url
+    )} ${scale}x`;
+  }, "");
+});
+</script>
+
+<template>
+  <div
+    class="absolute bottom-[74px] left-[35px] h-[41px] w-[41px] overflow-hidden rounded-[1.5px] bg-black"
+  >
+    <transition name="fade-photo" mode="out-in">
+      <img
+        :src="mainSrc"
+        :alt="props.slide.alt"
+        :srcset="mainSrcset"
+        class="h-full w-auto object-cover"
+        :key="props.slide.name + '-main'"
+      />
+    </transition>
+  </div>
+</template>
