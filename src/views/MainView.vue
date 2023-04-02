f9aed91c978bb17c0063b56fa787078016b055f7
Konstantin Savusia <savconstantine@gmail.com>
2023-04-03 01:36:42 +0600

    defineAsync for iphone mokup

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,14 +1,23 @@
 <script setup>
+import { defineAsyncComponent } from "vue";
+import Spinner from "@/ui/Spinner.vue";
+
 import IphonePhotoAcademyCopyBlock from "@/modules/IphonePhotoAcademyCopyBlock/index.vue";
-import IphoneCarousel from "@/modules/IphoneCarousel/index.vue";
-import IPSLogo from "@/assets/ips-logo.svg?component";
+import IPSLogoText from "@/assets/ips-logo-text.svg?component";
+
+const IphoneCarousel = defineAsyncComponent({
+  loader: () => import("@/modules/IphoneCarousel/index.vue"),
+  loadingComponent: Spinner,
+});
 </script>
 
 <template>
   <div
     class="flex w-full max-w-[393px] flex-col items-start justify-start gap-4 md:max-w-[1200px] md:flex-row md:justify-between"
   >
-    <IPSLogo class="absolute left-[20px] top-[62px] hidden h-[45px] md:block" />
+    <IPSLogoText
+      class="absolute left-[20px] top-[62px] hidden h-[45px] md:block"
+    />
     <IphonePhotoAcademyCopyBlock />
     <IphoneCarousel />
   </div>
