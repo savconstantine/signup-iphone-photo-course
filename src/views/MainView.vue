439d9b2b842b9c2f2784237adfb98ce2f9f61887
Konstantin Savusia <savconstantine@gmail.com>
2023-04-02 10:45:09 +0600

    mainView added logo and iphone carousel

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,12 +1,15 @@
 <script setup>
-import IphonePhotoAcademyCopyBlock from "@/modules/IphonePhotoAcademyCopyBlock/index.vue";
+import IphonePhotoAcademyCopyBlock from "@/modules/IphonePhotoAcademyCopyBlock/IphonePhotoAcademyCopyBlock.vue";
+import IphoneCarousel from "@/modules/IphoneCarousel/IphoneCarousel.vue";
+import IPSLogo from "@/assets/ips-logo.svg?component";
 </script>
 
 <template>
   <div
     class="flex w-full max-w-[393px] flex-col items-start justify-start gap-4 md:flex-row"
   >
+    <IPSLogo class="absolute left-[20px] top-[62px] hidden h-[45px] md:block" />
     <IphonePhotoAcademyCopyBlock />
-    <div>iphone mockup</div>
+    <IphoneCarousel />
   </div>
 </template>
