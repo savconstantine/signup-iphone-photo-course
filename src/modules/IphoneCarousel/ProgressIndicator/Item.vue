beea22181dabbd1ecef17bec44b3660a853fe2c3
Konstantin Savusia <savconstantine@gmail.com>
2023-04-03 00:47:43 +0600

    progressIndicator fix, after the first cycle, it doesn't work anymore. Now it is fixed.

--- src/modules/IphoneCarousel/ProgressIndicator/Item.vue
+++ src/modules/IphoneCarousel/ProgressIndicator/Item.vue
@@ -12,17 +12,17 @@ const fillerStyles = ref({ width: "0%" });
 watch(
   () => props.currentSlide,
   (newVal) => {
-    console.log("newVal", newVal);
-
     if (newVal === 0) {
       fillerStyles.value = { width: "0%" };
     }
 
     if (props.isCurrentSlide) {
-      fillerStyles.value = {
-        width: `100%`,
-        transition: "width 8s ease-in-out",
-      };
+      setTimeout(() => {
+        fillerStyles.value = {
+          width: `100%`,
+          transition: "width 8s ease-in-out",
+        };
+      }, 100);
     }
     if (props.isCurrentItemLessThenCurrentSlide) {
       fillerStyles.value = {
@@ -36,7 +36,7 @@ watch(
   <div class="h-[1px] w-full max-w-[50px] rounded-[1px] bg-[#3A3A3A]">
     <div
       :style="fillerStyles"
-      class="h-full w-[1%] rounded-[inherit] bg-[#EEECEC]"
+      class="h-full w-0 rounded-[inherit] bg-[#EEECEC]"
     ></div>
   </div>
 </template>
