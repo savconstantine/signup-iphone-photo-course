cd521d8e968e941ae0d424f1d7953861cefcb985
Konstantin Savusia <savconstantine@gmail.com>
2023-04-02 10:17:52 +0600

    iphone shutter button

---
+++ src/modules/IphoneCarousel/Iphone/ShutterButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useIphoneCarousel } from "@/stores/iPhoneCarousel";
+
+const iphoneCarouselStore = useIphoneCarousel();
+
+const takeAPicture = () => {
+  iphoneCarouselStore.clearTimeoutId();
+  iphoneCarouselStore.rollTheSlides();
+};
+</script>
+
+<template>
+  <button
+    type="button"
+    class="absolute bottom-[71px] left-[153px] h-[47px] w-[47px] rounded-full bg-white"
+    @click="takeAPicture"
+  ></button>
+</template>
