ab1ba9ab83ecf94e3bcb3f23a82ccd03f41e5ed9
Konstantin Savusia <savconstantine@gmail.com>
2023-04-02 10:18:30 +0600

    thankyou page

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,9 @@ const router = createRouter({
       component: MainView,
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      path: "/thank-you",
+      name: "thank-you",
+      component: () => import("../views/ThankYouView.vue"),
     },
   ],
 });
